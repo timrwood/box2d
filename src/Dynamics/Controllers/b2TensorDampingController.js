3ee8570e259e756eb3077968d34ea21f86a32426
Tim Wood <washwithcare@gmail.com>
2013-04-22 16:26:35 -0700

    b2TensorDampingController

---
+++ src/Dynamics/Controllers/b2TensorDampingController.js
@@ -0,0 +1,53 @@
+function b2TensorDampingController() {
+	b2Controller.apply(this, arguments);
+	this.T = new b2Mat22();
+	this.maxTimestep = 0;
+}
+
+b2TensorDampingController.prototype = extend(new b2Controller(), {
+	SetAxisAligned : function (xDamping, yDamping) {
+		xDamping = xDamping || 0;
+		yDamping = yDamping || 0;
+		this.T.col1.x = -xDamping;
+		this.T.col1.y = 0;
+		this.T.col2.x = 0;
+		this.T.col2.y = -yDamping;
+		if (xDamping > 0 || yDamping > 0) {
+			this.maxTimestep = 1 / Math.max(xDamping, yDamping);
+		} else {
+			this.maxTimestep = 0;
+		}
+	},
+
+	Step : function (step) {
+		var timestep = step.dt,
+			i,
+			body,
+			vel,
+			damping;
+
+		if (timestep <= Number.MIN_VALUE) {
+			return;
+		}
+
+		if (timestep > this.maxTimestep && this.maxTimestep > 0) {
+			timestep = this.maxTimestep;
+		}
+
+		for (i = this.m_bodyList; i; i = i.nextBody) {
+			body = i.body;
+
+			if (!body.IsAwake()) {
+				continue;
+			}
+
+			damping = body.GetWorldVector(b2Math.MulMV(this.T, body.GetLocalVector(body.GetLinearVelocity()))); // TODO: b2Vec2 reuse?
+
+			vel = body.GetLinearVelocity();
+			vel.x += damping.x * timestep;
+			vel.y += damping.y * timestep;
+
+			body.SetLinearVelocity(vel);
+		}
+	}
+});
--- src/todo.js
+++ src/todo.js
@@ -1,13 +1,4 @@
 
-/* Controllers */
-
-
-	function b2TensorDampingController() {
-		b2TensorDampingController.b2TensorDampingController.apply(this, arguments);
-	};
-	Box2D.Dynamics.Controllers.b2TensorDampingController = b2TensorDampingController;
-
-
 /* Joints */
 
 
@@ -202,51 +193,10 @@ Box2D.postDefs = [];
 		Box2D.Dynamics.Contacts.b2ContactSolver.s_worldManifold = new b2WorldManifold();
 		Box2D.Dynamics.Contacts.b2ContactSolver.s_psm = new b2PositionSolverManifold();
 	});
-})();
-(function () {
-
-
 	Box2D.postDefs.push(function () {
 		Box2D.Dynamics.Contacts.b2PositionSolverManifold.circlePointA = new b2Vec2();
 		Box2D.Dynamics.Contacts.b2PositionSolverManifold.circlePointB = new b2Vec2();
 	});
-})();
-(function () {
-
-	Box2D.inherit(b2TensorDampingController, Box2D.Dynamics.Controllers.b2Controller);
-	b2TensorDampingController.prototype.__super = Box2D.Dynamics.Controllers.b2Controller.prototype;
-	b2TensorDampingController.b2TensorDampingController = function () {
-		Box2D.Dynamics.Controllers.b2Controller.b2Controller.apply(this, arguments);
-		this.T = new b2Mat22();
-		this.maxTimestep = 0;
-	};
-	b2TensorDampingController.prototype.SetAxisAligned = function (xDamping, yDamping) {
-		if (xDamping === undefined) xDamping = 0;
-		if (yDamping === undefined) yDamping = 0;
-		this.T.col1.x = (-xDamping);
-		this.T.col1.y = 0;
-		this.T.col2.x = 0;
-		this.T.col2.y = (-yDamping);
-		if (xDamping > 0 || yDamping > 0) {
-			this.maxTimestep = 1 / Math.max(xDamping, yDamping);
-		}
-		else {
-			this.maxTimestep = 0;
-		}
-	}
-	b2TensorDampingController.prototype.Step = function (step) {
-		var timestep = step.dt;
-		if (timestep <= Number.MIN_VALUE) return;
-		if (timestep > this.maxTimestep && this.maxTimestep > 0) timestep = this.maxTimestep;
-		for (var i = this.m_bodyList; i; i = i.nextBody) {
-			var body = i.body;
-			if (!body.IsAwake()) {
-				continue;
-			}
-			var damping = body.GetWorldVector(b2Math.MulMV(this.T, body.GetLocalVector(body.GetLinearVelocity())));
-			body.SetLinearVelocity(new b2Vec2(body.GetLinearVelocity().x + damping.x * timestep, body.GetLinearVelocity().y + damping.y * timestep));
-		}
-	}
 })();
 (function () {
 
